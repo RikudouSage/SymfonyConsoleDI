e16b797dd1ff5e0bf243e983f080372142791e99
Dominik Chrastecky <dominik@chrastecky.cz>
2019-09-03 17:30:07 +0200

    add interface for single command apps

--- src/DependencyInjection/CommandLocatorCompilerPass.php
+++ src/DependencyInjection/CommandLocatorCompilerPass.php
@@ -2,19 +2,19 @@
 
 namespace App\DependencyInjection;
 
+use App\Interfaces\SingleCommandInterface;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
+use function call_user_func;
+use function is_a;
 
 class CommandLocatorCompilerPass implements CompilerPassInterface
 {
 
-    /**
-     * You can modify the container here before it is dumped to PHP code.
-     * @param ContainerBuilder $container
-     */
     public function process(ContainerBuilder $container)
     {
         $application = $container->getDefinition(Application::class);
@@ -22,7 +22,18 @@ class CommandLocatorCompilerPass implements CompilerPassInterface
         foreach ($container->getDefinitions() as $name => $definition) {
             if (is_a($definition->getClass(), Command::class, true)) {
                 $application->addMethodCall("add", [new Reference($name)]);
+
+                if (is_a($definition->getClass(), SingleCommandInterface::class, true)) {
+                    $this->setDefaultCommand($definition, $application);
+                }
             }
         }
+    }
+
+    private function setDefaultCommand(Definition $definition, Definition $application) {
+        $class = $definition->getClass();
+        $commandName = call_user_func([$class, 'getCommandName']);
+
+        $application->addMethodCall('setDefaultCommand', [$commandName, true]);
     }
 }
